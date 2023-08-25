8bcc7428b88be3f1c5e1f8882a47d872e8605741
kapadiyadarshan <darshan.kapadia.35@gmail.com>
2023-08-25 22:26:24 +0530

    Update

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
+
       ),
       darkTheme: ThemeData.dark(
         useMaterial3: true,
